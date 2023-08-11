a9c1c71148ba154f15b525f94aa2e4ef69654271
Chris Bell <christopher.bell@vention.cc>
2023-08-10 23:15:55 -0400

    Functional UI

---
+++ client/src/Slider.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import ReactSlider from "react-slider";
+
+const Slider = ({ minValue, maxValue, defaultValue, identifier, index }) => {
+  const [currentValue, setCurrentValue] = useState(defaultValue);
+
+  return (
+    <div>
+      <label htmlFor={identifier}>
+        {`Input ${identifier} ${index}: ${currentValue}`}
+      </label>
+      <ReactSlider
+        className="customSlider"
+        trackClassName="customSlider-track"
+        thumbClassName="customSlider-thumb"
+        min={minValue}
+        max={maxValue}
+        // defaultValue={defaultValue}
+        value={currentValue}
+        onChange={(value) => setCurrentValue(value)}
+      />
+    </div>
+  );
+};
+
+export default Slider;
